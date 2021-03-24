c771dc76334bff2d3acd68e664df0efd4e6ae438
leidy <leidy@192.168.0.8>
2021-03-24 10:53:07 -0400

    practice 1

--- src/ntk/group/one/TeamOneDemo.java
+++ src/ntk/group/one/TeamOneDemo.java
@@ -6,6 +6,16 @@ public class TeamOneDemo {
 		System.out.println("Team One ");
 		
 		System.out.println("What's Up Team");
+		
+		System.out.println("Faisal");
+		System.out.println("Faisal2");
+		
+		System.out.println("We will Be Working From This Class..");
+		
+		System.out.println("nice conversation guys <3");
+		
+		System.out.println("buuuu");
+		
 	}
 
 }
